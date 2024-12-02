bc671589da052369ac95f17398ef08b3d867dc70
ShernOC <sherlynea1994@hotmail.com>
2024-12-02 21:59:00 +0300

    anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+# your code goes here 
+class Anagram: 
+    words = ['listen','google', 'inlets', 'banana']
+    
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self,words):
+        match = [letter for letter in words if sorted  (letter) == sorted(self.word)]
+        return match 
+    
+    
+    
